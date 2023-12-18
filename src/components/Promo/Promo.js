b544d26b9203f03c64115324716f42640f7113cf
[Andrey Lyulekin] <[Lyulekin@mail.ru]>
2023-12-17 22:38:34 -0300

    refactor css with % between breakpoints #5

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,26 +1,24 @@
 export default function Promo() {
   return (
     <section className='promo'>
-      <div className='promo__container'>
-        <h1 className='promo__header'>Учебный проект студента факультета Веб-разработки.</h1>
-        <nav className='promo__nav-bar'>
-          <a
-            href='#about'
-            className='promo__nav util__button'>
-            О проекте
-          </a>
-          <a
-            href='#tech'
-            className='promo__nav util__button'>
-            Технологии
-          </a>
-          <a
-            href='#student'
-            className='promo__nav util__button'>
-            Студент
-          </a>
-        </nav>
-      </div>
+      <h1 className='promo__header'>Учебный проект студента факультета Веб-разработки.</h1>
+      <nav className='promo__nav-bar'>
+        <a
+          href='#about'
+          className='promo__nav util__button'>
+          О проекте
+        </a>
+        <a
+          href='#tech'
+          className='promo__nav util__button'>
+          Технологии
+        </a>
+        <a
+          href='#student'
+          className='promo__nav util__button'>
+          Студент
+        </a>
+      </nav>
     </section>
   );
 }
